8c68d0fb3f5f9d40c37b1687a0ac5f85c6055987
Vincenzo <laveneziana.vincenzo@gmail.com>
2020-04-09 17:36:34 +0200

    fine pulsante destro

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 $(document).ready(function () {
   
-
-
-
   var leftArrow = $(".left_arrow > a");
   var rightArrow = $(".right_arrow > a");
   var activeImg = $("img.active");
   var prevImg = $(".active").prev("img"); 
   var nextImg = $(".active").next("img");
+  
+  leftArrow.click(function(){
+    left();
+  });
+
+
+  rightArrow.click(function(){
+    right()
+  });
   
 
   function riuseVar() {
@@ -15,12 +21,7 @@ $(document).ready(function () {
     prevImg = $(".active").prev("img");
   }
 
-  leftArrow.click(function(){
 
-    left();
-
-  });
-  
   function left(){
     if(activeImg.hasClass("first")){
       prevImg = $("img.last");
@@ -31,9 +32,36 @@ $(document).ready(function () {
     riuseVar()
   }
 
-  rightArrow.click(function(){
-    console.log("yes right")
+
+  function right(){
+    if(activeImg.hasClass("last")){
+      nextImg = $("img.first");
+    }
+    activeImg.removeClass("active");
+    nextImg.addClass("active");
+    activeImg = $(".active");
+    riuseVar()
+  }
+
+
+  $(document).keydown(function (e) { 
+    console.log(e.keyCode);
+
+    if(e.keyCode == 37){
+      left();
+    } else if(e.keyCode == 39){
+      right();
+    }
+
+    //easter egg da implementare al momento non funzionante
+    if(e.keyCode == 86){
+      if(e.keyCode == 73){
+        if(e.keyCode == 78){
+          $("img.active").html('<img class="first active" src="https://media.giphy.com/media/llOz4zgQCyCoYaRmBq/giphy.gif" ">')
+        }
+      }
+    }
+    
   });
-
-
+  
 });
