e9af454be9397083c0863e92195c60fee53e29c2
Vincenzo <laveneziana.vincenzo@gmail.com>
2020-04-09 17:01:23 +0200

    Fine, pulsante sinistro

--- js/main.js
+++ js/main.js
@@ -1,1 +1,39 @@
-/** */
+$(document).ready(function () {
+  
+
+
+
+  var leftArrow = $(".left_arrow > a");
+  var rightArrow = $(".right_arrow > a");
+  var activeImg = $("img.active");
+  var prevImg = $(".active").prev("img"); 
+  var nextImg = $(".active").next("img");
+  
+
+  function riuseVar() {
+    nextImg = $(".active").next("img");
+    prevImg = $(".active").prev("img");
+  }
+
+  leftArrow.click(function(){
+
+    left();
+
+  });
+  
+  function left(){
+    if(activeImg.hasClass("first")){
+      prevImg = $("img.last");
+    }
+    activeImg.removeClass("active");
+    prevImg.addClass("active");
+    activeImg = $(".active");
+    riuseVar()
+  }
+
+  rightArrow.click(function(){
+    console.log("yes right")
+  });
+
+
+});
